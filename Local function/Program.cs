769868f6f59d228bc8e017e0bd5404a1a9817435
sonasol2 <57984322+sonasol2@users.noreply.github.com>
2024-01-31 18:08:23 +0200

    done

--- Local function/Program.cs
+++ Local function/Program.cs
@@ -4,7 +4,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        void Compare(int[] numbers1, int[] numbers2)
+        {
+            int numbers1Sum = Sum(numbers1);
+            int numbers2Sum = Sum(numbers2);
+
+            if (numbers1Sum > numbers2Sum)
+                Console.WriteLine("Сумма числе из массива Numbers1 больше");
+            else if (numbers1Sum < numbers2Sum)
+                Console.WriteLine("Сумма числе из массива Numbers2 больше");
+	        else
+                Console.WriteLine("Суммы чисел обоих массивов равны");
+
+            int Sum(int[] numbers)
+            
+	        {
+                int result = 0;
+                foreach (int number in numbers)
+                    result += number;
+                return result;
+	        }
+
+          
+        }
+        int[] numbers1 = { 1, 3, 4, 5 };
+        int[] numbers2 = { 1, 5, 6 };
+        Compare(numbers1, numbers2);
+
+        //  локальные функции это по сути методы внутри методов. Такие функции нельзя использовать вне метода
+        // также такие функции могут не иметь доступа к переменным метода если указать ключевое слово static
+
+    
     }
 }
 
